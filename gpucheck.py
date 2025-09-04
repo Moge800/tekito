f26e5232735780d31e20a182a7dcc01d26852c44
Moge800 <benom0210@gmail.com>
2025-09-05 02:47:16 +0900

    GPUデバイスの確認機能を新規追加し、利用可能なGPUをリスト表示するスクリプトを作成

---
+++ gpucheck.py
@@ -0,0 +1,14 @@
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+from tensorflow.python.client import device_lib
+import tensorflow as tf
+
+
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+
+
+print(get_available_devices())
+print("利用可能GPU: ", len(tf.config.list_physical_devices("GPU")))
